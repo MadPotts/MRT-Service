bc93482c243babf43e2ff751b94eeb1eb1fa7a67
Maddie Potts <65889270+MadPotts@users.noreply.github.com>
2022-02-20 02:23:40 -0700

    Add Solar Elevation Service

---
+++ SolarElevationService/IService1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Homework3Part3 {
+    [ServiceContract]
+    public interface IService1 {
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "/getSolarElevation/{lat}/{lon}")]
+        root getSolarElevation(string lat, string lon);
+    }
+
+    [DataContract]
+    public class root {
+        [DataMember]
+        public solarElevationForecast[] forecast { get; set; }
+    }
+
+    [DataContract]
+    public class solarElevationForecast {
+        [DataMember]
+        public float solarElevation { get; set; }
+        [DataMember]
+        public string date { get; set; }
+    }
+
+    [DataContract]
+    public class Rootobject {
+        [DataMember]
+        public Location location { get; set; }
+        [DataMember]
+        public string date { get; set; }
+        [DataMember]
+        public string current_time { get; set; }
+        [DataMember]
+        public string sunrise { get; set; }
+        [DataMember]
+        public string sunset { get; set; }
+        [DataMember]
+        public string sun_status { get; set; }
+        [DataMember]
+        public string solar_noon { get; set; }
+        [DataMember]
+        public string day_length { get; set; }
+        [DataMember]
+        public float sun_altitude { get; set; }
+        [DataMember]
+        public float sun_distance { get; set; }
+        [DataMember]
+        public float sun_azimuth { get; set; }
+        [DataMember]
+        public string moonrise { get; set; }
+        [DataMember]
+        public string moonset { get; set; }
+        [DataMember]
+        public string moon_status { get; set; }
+        [DataMember]
+        public float moon_altitude { get; set; }
+        [DataMember]
+        public float moon_distance { get; set; }
+        [DataMember]
+        public float moon_azimuth { get; set; }
+        [DataMember]
+        public float moon_parallactic_angle { get; set; }
+    }
+
+    [DataContract]
+    public class Location {
+        [DataMember]
+        public float latitude { get; set; }
+        [DataMember]
+        public float longitude { get; set; }
+    }
+}
---
+++ SolarElevationService/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Homework3Part3 {
+    public class Service1 : IService1 {
+        //this service gets the solar elevation at a given location at the current time
+        //for the next 7 days
+        public root getSolarElevation(string lat, string lon) {
+            //get current date
+            string date = DateTime.UtcNow.ToString("yyyy-MM-dd");
+
+            string url = "https://api.ipgeolocation.io/astronomy?apiKey=483864a7776f42dd8452acd892468047&lat=";
+
+            url += lat + "&long=" + lon + "&date=";
+
+            //create forecast object and initialize
+            solarElevationForecast[] forecast = new solarElevationForecast[7];
+            for (int i = 0; i < 7; i++) {
+                forecast[i] = new solarElevationForecast();
+            }
+            root r = new root();
+
+            for (int i = 0; i < 7; i++) {
+                url += date;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader sreader = new StreamReader(dataStream);
+                string responsereader = sreader.ReadToEnd();
+                response.Close();
+
+                Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(responsereader);
+
+                forecast[i].solarElevation = obj.sun_altitude;
+                forecast[i].date = date;
+
+                //keep requesting info for subsequent days
+                date = DateTime.UtcNow.AddDays(i + 1).ToString("yyyy-MM-dd");
+            }
+
+            r.forecast = forecast;
+
+            return r;
+        }
+    }
+}
