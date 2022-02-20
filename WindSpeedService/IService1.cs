495f5f919cf1127f7d5a45bde1a8f84a3c2388c6
Maddie Potts <65889270+MadPotts@users.noreply.github.com>
2022-02-20 02:25:42 -0700

    Add Wind Speed Service

---
+++ WindSpeedService/IService1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WindService {
+    [ServiceContract]
+    public interface IService1 {
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "/getWindSpeed/{lat}/{lon}")]
+        root getWindSpeed(string lat, string lon);
+    }
+
+    //my objects
+    [DataContract]
+    public class root {
+        [DataMember]
+        public windSpeedForecast[] forecast { get; set; }
+    }
+
+    [DataContract]
+    public class windSpeedForecast {
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public string windSpeedDate { get; set; }
+    }
+
+    //other api's objects
+    [DataContract]
+    public class Rootobject {
+        [DataMember]
+        public object error { get; set; }
+        [DataMember]
+        public Data data { get; set; }
+    }
+
+    [DataContract]
+    public class Data {
+        [DataMember]
+        public string timezone { get; set; }
+        [DataMember]
+        public Current current { get; set; }
+        [DataMember]
+        public Hourly[] hourly { get; set; }
+        [DataMember]
+        public Daily[] daily { get; set; }
+    }
+
+    [DataContract]
+    public class Current {
+        [DataMember]
+        public DateTime time { get; set; }
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public float temperature { get; set; }
+        [DataMember]
+        public float temperatureMin { get; set; }
+        [DataMember]
+        public float temperatureMax { get; set; }
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public float windGustsSpeed { get; set; }
+        [DataMember]
+        public float windBearing { get; set; }
+        [DataMember]
+        public float relHumidity { get; set; }
+        [DataMember]
+        public int cloudCover { get; set; }
+        [DataMember]
+        public float preasure { get; set; }
+        [DataMember]
+        public int totalPrecipitation { get; set; }
+        [DataMember]
+        public int rain { get; set; }
+        [DataMember]
+        public int snow { get; set; }
+        [DataMember]
+        public float uvIndex { get; set; }
+        [DataMember]
+        public float airQualityIndex { get; set; }
+    }
+
+    [DataContract]
+    public class Hourly {
+        [DataMember]
+        public DateTime time { get; set; }
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public float temperature { get; set; }
+        [DataMember]
+        public float temperatureMin { get; set; }
+        [DataMember]
+        public float temperatureMax { get; set; }
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public float windGustsSpeed { get; set; }
+        [DataMember]
+        public float windBearing { get; set; }
+        [DataMember]
+        public float relHumidity { get; set; }
+        [DataMember]
+        public float cloudCover { get; set; }
+        [DataMember]
+        public float preasure { get; set; }
+        [DataMember]
+        public float totalPrecipitation { get; set; }
+        [DataMember]
+        public float rain { get; set; }
+        [DataMember]
+        public int snow { get; set; }
+        [DataMember]
+        public float? uvIndex { get; set; }
+        [DataMember]
+        public float? airQualityIndex { get; set; }
+    }
+
+    [DataContract]
+    public class Daily {
+        [DataMember]
+        public DateTime time { get; set; }
+        [DataMember]
+        public Sun sun { get; set; }
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public float temperature { get; set; }
+        [DataMember]
+        public float temperatureMin { get; set; }
+        [DataMember]
+        public float temperatureMax { get; set; }
+        [DataMember]
+        public float windSpeed { get; set; }
+        [DataMember]
+        public float windSpeedMax { get; set; }
+        [DataMember]
+        public float windBearing { get; set; }
+        [DataMember]
+        public float relHumidity { get; set; }
+        [DataMember]
+        public float cloudCover { get; set; }
+        [DataMember]
+        public float preasure { get; set; }
+        [DataMember]
+        public float totalPrecipitation { get; set; }
+        [DataMember]
+        public float rain { get; set; }
+        [DataMember]
+        public int snow { get; set; }
+        [DataMember]
+        public float? uvIndexMax { get; set; }
+    }
+
+    [DataContract]
+    public class Sun {
+        [DataMember]
+        public DateTime sunrise { get; set; }
+        [DataMember]
+        public DateTime sunset { get; set; }
+    }
+}
---
+++ WindSpeedService/Service1.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WindService {
+    public class Service1 : IService1 {
+
+        //this method gets the data from the troposphere api with the given latitude
+        //and longitude, and returns the array of wind speeds in that area for the next week
+        public root getWindSpeed(string lat, string lon) {
+            string url = "https://api.troposphere.io/forecast/";
+            url += lat;
+            url += ",";
+            url += lon;
+            url += "?token=e93836f68aba12f0c61dbd3e068f0493f775cf5fdecf0c4db8";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+
+            Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(responsereader);
+
+            //create objects and initialize
+            root r = new root();
+            windSpeedForecast[] forecast = new windSpeedForecast[7];
+            for (int i = 0; i < 7; i++) {
+                forecast[i] = new windSpeedForecast();
+            }
+
+            for (int i = 0; i < 7; i++) {
+                forecast[i].windSpeed = obj.data.daily[i].windSpeed;
+
+                DateTime date = obj.data.daily[i].time.Date;
+                string dateStr = date.ToString("MM-dd-yyyy");
+                forecast[i].windSpeedDate = dateStr;
+            }
+            r.forecast = forecast;
+
+            return r;
+        }
+    }
+}
